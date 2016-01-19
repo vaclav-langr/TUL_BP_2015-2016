3614f897ea996beb8ba3bbb6d23ac86edf0fbc5c
VaclavLangr <vendavenda3@gmail.com>
2016-01-19 09:30:07 +0100

    Simulace Monte Carlo s ukládáním do objektu MAT.

---
+++ Simulace_struktura/ForwardBackward.m
@@ -0,0 +1,27 @@
+function [ x_n ] = ForwardBackward( y, A, tau, odchylka, kroky )
+x_n = zeros(size(A, 2), 1);
+
+alfa = 0.1;
+
+for i = 1:kroky
+    grad = -2*A'*(y-A*x_n);
+    
+    y_n = x_n - alfa * grad;
+    
+    x_n_1 = x_n;
+    x_n = x_n + 1 * (prox(tau * alfa, y_n) - x_n);
+    
+    if podminka1(x_n, y, A, tau, odchylka)
+        if podminka2(x_n, y, A, tau, odchylka)
+            if (sum(isnan(x_n)) > 0)
+                x_n = x_n_1;
+            end
+            break
+        end
+    end
+    
+    s = x_n - x_n_1;
+    
+    alfa = 1 / (norm(A'*A*s,1)^2/norm(A*s,1)^2);
+end
+end
---
+++ Simulace_struktura/simulace.m
@@ -0,0 +1,71 @@
+clear all
+close all
+clc
+
+i = 0;
+zaloha = dir('*.mat');
+
+if ~isempty(zaloha)
+    konec = false;
+    while ~konec
+        for soubor=1:length(zaloha)
+            disp(['[' num2str(soubor) '] - ' zaloha(soubor).name])
+        end
+        disp('[0] - None')
+        vstup = input('Select file: ');
+        if vstup == 0
+            konec = true;
+            startI = 1;
+        elseif (vstup >= 1 && vstup <= length(zaloha))
+            konec = true;
+            load(zaloha(vstup).name)
+            vstup = 1;
+            if exist('i','var') && i > 0
+                startI = i+1;
+            else
+                startI = struktura.nastaveni.pocet_opakovani + 1;
+            end
+        end
+    end
+else
+    vstup = 0;
+end
+if vstup == 0
+    struktura = struct();
+    struktura.nastaveni.nazev_zalohy = ['vysledek-' datestr(now,'ddmmyy-HHMMSS') '.mat'];
+    if strcmp(getenv('os'),'Windows_NT')
+        struktura.nastaveni.command = ['python ' pwd '\main.py'];
+    else
+        struktura.nastaveni.command = ['python ' pwd '/main.py'];
+    end
+    struktura.nastaveni.command = [struktura.nastaveni.command ' -t "MATLAB Simulation completed" -b "Simulation completed"'];
+    
+    struktura.nastaveni.pocet_tau = 100;
+    struktura.nastaveni.pocet_prvku = 100;
+    struktura.nastaveni.pocet_radku = floor(struktura.nastaveni.pocet_prvku/2);
+    struktura.nastaveni.pocet_opakovani = 1000;
+    struktura.nastaveni.odchylka = 10^(-6);
+    struktura.x_orig = full(sprandn(struktura.nastaveni.pocet_prvku,1,0.1));
+    struktura.A = randn(struktura.nastaveni.pocet_radku, struktura.nastaveni.pocet_prvku, struktura.nastaveni.pocet_opakovani);
+    struktura.tau = logspace(-6,2.7, struktura.nastaveni.pocet_tau);
+    struktura.kroky = zeros(struktura.nastaveni.pocet_opakovani, struktura.nastaveni.pocet_tau);
+    
+    startI = 1;
+end
+
+for i = startI:struktura.nastaveni.pocet_opakovani
+    y_orig = struktura.A(:,:,i)*struktura.x_orig;
+    for j = 1:struktura.nastaveni.pocet_tau
+        x_compute = ForwardBackward(y_orig,struktura.A(:,:,i),struktura.tau(j),struktura.nastaveni.odchylka,3000);
+        struktura.kroky(i, j) = norm(struktura.x_orig - x_compute, 2)^2;
+    end
+    save(struktura.nastaveni.nazev_zalohy, 'struktura', 'i');
+end
+save(struktura.nastaveni.nazev_zalohy, 'struktura')
+plot(struktura.tau, sum(struktura.kroky)/struktura.nastaveni.pocet_opakovani)
+set(gca,'xscale','log')
+set(gca,'yscale','log')
+
+system(struktura.nastaveni.command);
+
+clear ans command i j startI vstup x_compute y_orig zaloha konec soubor
